33cfdb8b2737dc189e39097a07068e7211325741
Patric Gruber <patric.gruber@gmx.at>
2019-07-28 12:28:36 +0200

    added documentation to terminal wrapper and removed parameter escaping

--- src/wrapper/TerminalWrapper.java
+++ src/wrapper/TerminalWrapper.java
@@ -11,9 +11,18 @@ import java.util.List;
  * @author patric
  */
 public class TerminalWrapper {
+    /**
+     * Executes the given command and returns the process. 
+     * The method already calls ``waitFor`` on the process
+     * 
+     * @param commandList is the command list that is given to the process on creation
+     * @return the created process
+     * @throws WrapperException will be thrown if there is a problem during 
+     *                          the process creation or waiting
+     */
      private static Process execute(final List<String> commandList) 
             throws WrapperException{
-         
+
         final String command = 
         commandList.stream()
             .map(s -> s+" ")
@@ -21,6 +30,7 @@ public class TerminalWrapper {
             .orElseThrow();
         
         final Process p;
+        
         try {
             p = new ProcessBuilder(commandList).start();
         } catch (IOException ioe) {
@@ -34,9 +44,15 @@ public class TerminalWrapper {
             throw new WrapperException("Couldn't wait for process for command:"
                     + " \""+command+"\"");
         }
+        
         return p;
     }
-    
+     
+    /**
+     * @return a List of all available tablet names
+     * @throws WrapperException if there was a problem during execution 
+     *                          of the command ``xsetwacom list devices``
+     */
     public static List<String> getAllTabletNames() throws WrapperException{
         final List<String> commandList = Arrays.asList("xsetwacom", "list", "devices");
         final Process p = execute(commandList);
@@ -62,13 +78,18 @@ public class TerminalWrapper {
         return tabletNames;
     }
 
+    /**
+     * Sets a property of the tablet with the given name to the given value
+     * 
+     * @param tabletName is the name of the tablet
+     * @param property is the name of the property
+     * @param value is the value the property should be set to
+     * @throws WrapperException will be thrown if there is a problem during command execution
+     */
     public static void set(final String tabletName, final String property, final String value) 
             throws WrapperException {
-        final String escapedTabletName = '"'+tabletName+'"';
-        final String escapedValue = '"'+value+'"';
-        
         final List<String> commandList = 
-                Arrays.asList("xsetwacom", "set", escapedTabletName, property, escapedValue);
+                Arrays.asList("xsetwacom", "set", tabletName, property, value);
         
         final String command = 
                 commandList.stream()
@@ -88,13 +109,18 @@ public class TerminalWrapper {
         }
     }
 
+    /**
+     * Returns the value of a property of the given tablet
+     * 
+     * @param tabletName is the name of the tablet
+     * @param property is the name of the property
+     * @return the value of the property of the given tablet
+     * @throws WrapperException will be thrown if there is a problem during the command execution
+     */
     public static String get(String tabletName, String property) 
             throws WrapperException{
-        
-        final String escapedTabletName = '"'+tabletName+'"';
-        
         final List<String> commandList = 
-                Arrays.asList("xsetwacom", "get", escapedTabletName, property);
+                Arrays.asList("xsetwacom", "get", tabletName, property);
         
         final String command = commandList.stream()
                                         .map(s -> s+" ")
@@ -108,9 +134,8 @@ public class TerminalWrapper {
         final StringBuilder out = new StringBuilder();
         try {
             while (isr.ready()) out.append((char)isr.read());
-        } catch (IOException ioe) {
-            out.delete(0, out.length());
-        }
+        } catch (IOException ioe) {}
+        
         
         final String returnedString = out.toString().trim();
         
